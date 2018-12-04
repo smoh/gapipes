1b2bf9c382ab9c17723017ae826d13de84b493a3
Semyeong Oh <semyeong.oh@gmail.com>
2018-12-04 15:27:09 +0000

    Remove validation decorator
    - This decorator removes docsting from wrapped function, so at least it
      should use wraps from python library.
    - But even using that will still hide function signature.

--- gapipes/pipes.py
+++ gapipes/pipes.py
@@ -8,26 +8,18 @@ import astropy.coordinates as coord
 import astropy.units as u
 
 __all__ = [
-    'pipe', 'calculate_vtan_error', 'add_vtan_errors', 'add_vtan',
+    'calculate_vtan_error', 'add_vtan_errors', 'add_vtan',
     'make_icrs', 'add_x', 'add_xv', 'add_a_g_error', 'add_gMag', 'flag_good_phot']
 
 
-def pipe(func):
-    """Decorator to validate the first argument is pandas.DataFrame"""
-    def newfunc(*args, **kwargs):
-        if not isinstance(args[0], pd.DataFrame):
-            raise ValueError("df should be a pandas.DataFrame")
-        return func(*args, **kwargs)
-    return newfunc
-
-
-@pipe
 def calculate_vtan_error(df):
     """
     Calculate tangential velocity errors with small error propagation
 
     Returns (vra_error, vdec_error) in km/s
     """
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df should be a pandas.DataFrame")
     vra_error = np.hypot(df.pmra_error/df.parallax,
                          df.parallax_error/df.parallax**2*df.pmra)*4.74
     vdec_error = np.hypot(df.pmdec_error/df.parallax,
@@ -35,9 +27,10 @@ def calculate_vtan_error(df):
     return vra_error, vdec_error
 
 
-@pipe
 def add_vtan_errors(df):
     """ Add 'vra_error' and 'vdec_error' columns to Gaia DataFrame """
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df should be a pandas.DataFrame")
     df = df.copy()
     vra_error, vdec_error = calculate_vtan_error(df)
     df['vra_error'] = vra_error
@@ -45,9 +38,10 @@ def add_vtan_errors(df):
     return df
 
 
-@pipe
 def add_vtan(df):
     """ Add 'vra' and 'vdec' columns to Gaia DataFrame """
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df should be a pandas.DataFrame")
     df = df.copy()
     vra, vdec = df.pmra/df.parallax*4.74, df.pmdec/df.parallax*4.74
     df['vra'] = vra
@@ -55,9 +49,10 @@ def add_vtan(df):
     return df
 
 
-@pipe
 def make_icrs(df, include_pm_rv=True):
     """Returns ICRS instance from Gaia DataFrame"""
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df should be a pandas.DataFrame")
     if not include_pm_rv:
         return coord.ICRS(
             ra=df.ra.values*u.deg,        
@@ -72,16 +67,16 @@ def make_icrs(df, include_pm_rv=True):
             radial_velocity=df.radial_velocity.values*u.km/u.s)
 
 
-@pipe
 def add_x(df, frame, unit=u.pc):
     """Add cartesian coordinates `x`, `y`, `z` of a given `frame`"""
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df should be a pandas.DataFrame")
     df = df.copy()
     c = make_icrs(df, include_pm_rv=False).transform_to(frame)
     df['x'], df['y'], df['z'] = c.cartesian.xyz.to(unit).value
     return df
 
 
-@pipe
 def add_xv(df, frame, unit=u.pc):
     """
     Add cartesian coordinates x, y, z, vx, vy, vz for a given `frame`
@@ -93,6 +88,8 @@ def add_xv(df, frame, unit=u.pc):
     
     Returns df with x, y, z, vx, vy, vz columns added.
     """
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df should be a pandas.DataFrame")
     df = df.copy()
     c = make_icrs(df).transform_to(frame)
     df['x'], df['y'], df['z'] = c.cartesian.xyz.to(unit).value
@@ -100,12 +97,13 @@ def add_xv(df, frame, unit=u.pc):
     return df
 
 
-@pipe
 def add_a_g_error(df):
     """Extract lerr and uerr of a_g from percentiles.
 
     Returns df with a_g_lerr, a_g_uerr columns added.
     """
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df should be a pandas.DataFrame")
     df = df.copy()
     lerr = df['a_g_val'] - df['a_g_percentile_lower']
     uerr = df['a_g_percentile_upper'] - df['a_g_val']
@@ -113,25 +111,28 @@ def add_a_g_error(df):
     return df
 
 
-@pipe
 def add_distmod(df):
     """Add distance modulus `distmod`"""
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df should be a pandas.DataFrame")
     df = df.copy()
     df['distmod'] = 5*np.log10(df['parallax']) - 10
     return df
 
 
-@pipe
 def add_gMag(df):
     """Add absolute G Mag `gMag`"""
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df should be a pandas.DataFrame")
     df = df.copy()
     df['gMag'] = df['phot_g_mean_mag'] + 5*np.log10(df['parallax']) - 10
     return df
 
 
-@pipe
 def flag_good_phot(df):
     """Add """
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df should be a pandas.DataFrame")
     df = df.copy()
     good_phot = ((df.phot_bp_rp_excess_factor > 1+0.015*df.bp_rp**2)
                  & (df.phot_bp_rp_excess_factor < 1.3+0.06*df.bp_rp**2))
