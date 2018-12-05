9851f9aa94ea967b39a1c858182f9d8b393f5cc8
Semyeong Oh <semyeong.oh@gmail.com>
2018-12-05 19:59:14 +0000

    Add default instance to init

--- gapipes/gaia/__init__.py
+++ gapipes/gaia/__init__.py
@@ -1,3 +1,8 @@
 from .core import Tap, GaiaTapPlus
 
-__all__ = ['Tap', 'GaiaTapPlus']
+gaia = GaiaTapPlus.from_url(
+    "https://gea.esac.esa.int/tap-server/tap",
+    server_context='tap-server',
+    upload_context='Upload')
+
+__all__ = ['Tap', 'GaiaTapPlus', 'gaia']
