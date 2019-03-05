894c42b9b5f4c059757ae04ca0eb0e6a0e4d11d3
Semyeong Oh <semyeong.oh@gmail.com>
2019-03-05 22:08:15 +0000

    try docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
-    'nbsphinx'
+    'nbsphinx',
 ]
 
 napoleon_google_docstring = False
@@ -84,17 +84,21 @@ pygments_style = None
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-# html_theme = 'python_docs_theme'
-html_theme = 'sphinx_rtd_theme'
-#html_theme = 'alabaster'
+
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'analytics_id': 'UA-37308551-2'
+    'analytics_id': 'UA-37308551-2',
+    'fixed_sidebar': True,
+    'sidebar_width': '260px',
+    'page_width': '1200px',
+    'link_hover': '#FF851B',
+    'code_font_size': '0.8rem'
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
