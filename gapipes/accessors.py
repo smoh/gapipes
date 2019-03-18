cb23c0c4f8945692a08ab85fbeaba20116bb38c3
Semyeong Oh <semyeong.oh@gmail.com>
2019-03-18 13:57:39 +0000

    add license and update module

--- gapipes/accessors.py
+++ gapipes/accessors.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 import numpy as np
+import astropy.coordinates as coord
+import astropy.units as u
 
 @pd.api.extensions.register_dataframe_accessor("g")
 class GaiaAccessor(object):
@@ -18,12 +20,19 @@ class GaiaAccessor(object):
 
     @property
     def icrs(self):
-        # return the geographic center point of this DataFrame
-        ra, dec, parallax = self._df[['ra', 'dec', 'parallax']].values.T
         if self._icrs is None:
-            self._icrs = coord.ICRS(ra*u.deg, dec*u.deg, 1e3/parallax*u.pc)
+            req = set(['ra', 'dec', 'parallax', 'pmra', 'pmdec', 'radial_velocity'])
+            columns = set(self._df.columns.values)
+            if req < columns:
+                df = self._df
+                c = coord.ICRS(
+                    ra=df['ra'].values*u.deg,
+                    dec=df['dec'].values*u.deg,
+                    distance=1000./df['parallax'].values*u.pc,
+                    pm_ra_cosdec=df['pmra'].values*u.mas/u.year,
+                    pm_dec=df['pmdec'].values*u.mas/u.year,
+                    radial_velocity=df['radial_velocity'].values*u.km/u.s)
+                self._icrs = c
+            else:
+                raise AttributeError("columns missing")
         return self._icrs
-
-    def plot(self):
-        # plot this array's data on a map, e.g., using Cartopy
-        pass
